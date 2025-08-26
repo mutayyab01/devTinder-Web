50efacbdf76d2306b1b6c59c8b7387639910a8c1
mutayyab01 <mutayyab786yo@gmail.com>
2025-08-26 17:01:42 +0500

    Implement routing and layout structure with Navbar, Body, and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+import Body from "./Components/Body";
 
+function App() {
   return (
     <>
-      <h1>Mutayyab Imran</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,178 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer sm:footer-horizontal bg-base-300  p-10 fixed bottom-0 w-full">
+        <aside>
+          <img src="assests/devtinder.png" alt="Logo" width={100} />
+          <p>
+            Created By Mutayyab Imran
+            <br />
+          </p>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Social</h6>
+          <div className="grid grid-flow-col gap-4">
+            <a href="https://github.com/mutayyab01" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill="currentColor"
+                  d="M8 0c4.42 0 8 3.58 8 8a8.01 8.01 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38c0-.27.01-1.13.01-2.2c0-.75-.25-1.23-.54-1.48c1.78-.2 3.65-.88 3.65-3.95c0-.88-.31-1.59-.82-2.15c.08-.2.36-1.02-.08-2.12c0 0-.67-.22-2.2.82c-.64-.18-1.32-.27-2-.27s-1.36.09-2 .27c-1.53-1.03-2.2-.82-2.2-.82c-.44 1.1-.16 1.92-.08 2.12c-.51.56-.82 1.28-.82 2.15c0 3.06 1.86 3.75 3.64 3.95c-.23.2-.44.55-.51 1.07c-.46.21-1.61.55-2.33-.66c-.15-.24-.6-.83-1.23-.82c-.67.01-.27.38.01.53c.34.19.73.9.82 1.13c.16.45.68 1.31 2.69.94c0 .67.01 1.3.01 1.49c0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8"
+                />
+              </svg>
+            </a>
+            <a href="https://www.linkedin.com/in/mutayyab01" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  fill-rule="evenodd"
+                  d="M1 2.838A1.84 1.84 0 0 1 2.838 1H21.16A1.837 1.837 0 0 1 23 2.838V21.16A1.84 1.84 0 0 1 21.161 23H2.838A1.84 1.84 0 0 1 1 21.161zm8.708 6.55h2.979v1.496c.43-.86 1.53-1.634 3.183-1.634c3.169 0 3.92 1.713 3.92 4.856v5.822h-3.207v-5.106c0-1.79-.43-2.8-1.522-2.8c-1.515 0-2.145 1.089-2.145 2.8v5.106H9.708zm-5.5 10.403h3.208V9.25H4.208zM7.875 5.812a2.063 2.063 0 1 1-4.125 0a2.063 2.063 0 0 1 4.125 0"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </a>
+            <a href="https://facebook.com/mutayyab01" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 12 12"
+              >
+                <path
+                  fill="currentColor"
+                  d="M6 0a6 6 0 0 1 .813 11.945V7.63h1.552l.244-1.585H6.812v-.867c0-.658.214-1.242.827-1.242h.985V2.55c-.173-.024-.538-.075-1.23-.075c-1.444 0-2.29.767-2.29 2.513v1.055H3.618v1.585h1.484v4.304A6.001 6.001 0 0 1 6 0"
+                />
+              </svg>
+            </a>
+          </div>
+          <div className="grid grid-flow-col gap-4">
+            <a href="https://x.com/mutayyab01" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <g fill="currentColor">
+                  <path d="M1 2h2.5L3.5 2h-2.5zM5.5 2h2.5L7.2 2h-2.5z">
+                    <animate
+                      fill="freeze"
+                      attributeName="d"
+                      dur="0.4s"
+                      values="M1 2h2.5L3.5 2h-2.5zM5.5 2h2.5L7.2 2h-2.5z;M1 2h2.5L18.5 22h-2.5zM5.5 2h2.5L23 22h-2.5z"
+                    />
+                  </path>
+                  <path d="M3 2h5v0h-5zM16 22h5v0h-5z">
+                    <animate
+                      fill="freeze"
+                      attributeName="d"
+                      begin="0.4s"
+                      dur="0.4s"
+                      values="M3 2h5v0h-5zM16 22h5v0h-5z;M3 2h5v2h-5zM16 22h5v-2h-5z"
+                    />
+                  </path>
+                  <path d="M18.5 2h3.5L22 2h-3.5z">
+                    <animate
+                      fill="freeze"
+                      attributeName="d"
+                      begin="0.5s"
+                      dur="0.4s"
+                      values="M18.5 2h3.5L22 2h-3.5z;M18.5 2h3.5L5 22h-3.5z"
+                    />
+                  </path>
+                </g>
+              </svg>
+            </a>
+            <a href="https://www.youtube.com/@mutayyab01" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <mask id="SVGsyA92bmM">
+                  <g
+                    fill="none"
+                    fill-opacity="0"
+                    stroke="#fff"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                  >
+                    <path
+                      fill="#fff"
+                      stroke-dasharray="64"
+                      stroke-dashoffset="64"
+                      d="M12 5c9 0 9 0 9 7c0 7 0 7 -9 7c-9 0 -9 0 -9 -7c0 -7 0 -7 9 -7Z"
+                    >
+                      <animate
+                        fill="freeze"
+                        attributeName="fill-opacity"
+                        begin="2.34s"
+                        dur="1.95s"
+                        values="0;1"
+                      />
+                      <animate
+                        fill="freeze"
+                        attributeName="stroke-dashoffset"
+                        dur="2.34s"
+                        values="64;0"
+                      />
+                    </path>
+                    <path fill="#000" stroke="none" d="M12 11L12 12L12 13z">
+                      <animate
+                        fill="freeze"
+                        attributeName="d"
+                        begin="4.29s"
+                        dur="0.78s"
+                        values="M12 11L12 12L12 13z;M10 8.5L16 12L10 15.5z"
+                      />
+                      <set
+                        fill="freeze"
+                        attributeName="fill-opacity"
+                        begin="4.29s"
+                        to="1"
+                      />
+                    </path>
+                  </g>
+                </mask>
+                <rect
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  mask="url(#SVGsyA92bmM)"
+                />
+              </svg>
+            </a>
+            <a href="https://instagram.com/mutayyab01" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"
+                />
+              </svg>{" "}
+            </a>
+          </div>
+        </nav>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
